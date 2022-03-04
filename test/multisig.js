3dec381177bc915dcb4b0489da883dbd56ffbc26
LidoKing <kinglido203@gmail.com>
2022-03-04 10:49:55 +0800

    using wrong way to get returned value from createTransaction()

--- test/multisig.js
+++ test/multisig.js
@@ -63,18 +63,6 @@ describe("MultiSig", function () {
     expect(tx.creator).to.equal(addr1);
   });
 
-  it("should not create a transaction that exceeds wallet balance", async () => {
-    const {multisig, signer1, addr1, addr6} = await loadFixture(fixture);
-
-    // Deposit to contract
-    let amount = await ethers.utils.parseEther("20"); // eth to wei
-    await signer1.sendTransaction({from: addr1, to: multisig.address, value: amount});
-
-    // Create tx
-    await utils.shouldThrow(multisig.connect(signer1).createTransaction(addr6, 30));
-
-  });
-
   it("should confirm transaction", async () => {
     const {multisig, signer1, signer2, signer3, addr1, addr6} = await loadFixture(fixture);
 
@@ -175,5 +163,42 @@ describe("MultiSig", function () {
     await utils.shouldThrow(multisig.connect(signer1).executeTransaction(id));
   });
 
-  it("should ");
+  it("should simulate real situation", async () => {
+    const {multisig, signer1, signer2, signer3, signer4, signer5, signer6, addr1, addr2, addr6} = await loadFixture(fixture);
+
+    // Member1 deposit to contract
+    let amount = await ethers.utils.parseEther("100"); // eth to wei
+    await signer1.sendTransaction({from: addr1, to: multisig.address, value: amount});
+
+    // Member1 creates tx1
+    let result1 = await multisig.connect(signer1).createTransaction(addr6, 10);
+    let tx1id = result1.value.toNumber();
+    console.log(tx1id);
+    // Member2 confirms tx1
+    await multisig.connect(signer2).confirmTransaction(tx1id);
+    // Member2 creates tx2
+    let result2 = await multisig.connect(signer2).createTransaction(addr2, 50);
+    let tx2id = result2.value.toNumber();
+    console.log(tx2id);
+    // Some random guy (signer6) tries to confirm tx2
+    await utils.shouldThrow(multisig.connect(signer6).confirmTransaction(tx2id));
+    // Member2 revokes tx1
+    await multisig.connect(signer2).revokeConfirmation(tx1id);
+    // Member3 confirms tx1 and rejects tx2
+    await multisig.connect(signer3).confirmTransaction(tx1id);
+    //await multisig.connect(signer3).rejectTransaction(tx2id);
+    //console.log("safe");
+    // Member5 confirms tx1 and rejects tx2
+    await multisig.connect(signer5).confirmTransaction(tx1id);
+    //await multisig.connect(signer5).rejectTransaction(tx2id);
+    console.log("safe");
+    // Member1 executes tx1
+    await multisig.connect(signer1).executeTransaction(tx1id);
+    let tx1 = await multisig.getTransaction(tx1id);
+    expect(tx1.executed).to.equal(true);
+    // Member4 rejects tx2
+    await multisig.connect(signer4).rejectTransaction(tx2id);
+    let tx2 = await multisig.getTransaction(tx2id);
+    expect(tx2.rejected).to.equal(true);
+  });
 });

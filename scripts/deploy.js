9a853909305b6fc19cf298eaa69c5be9b5ed5fce
LidoKing <kinglido203@gmail.com>
2022-03-04 23:02:40 +0800

    deployment config

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,19 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const MultiSig = await hre.ethers.getContractFactory("MultiSig");
+  const multisig = await MultiSig.deploy(
+    [
+      '0xDaBd21Ed3aD4493512f5b75D7A5b9E679cc782bA',
+      '0x185515138a13C340d0b842af33256De14f8eA14F',
+      '0x983bC8675b387aC0b0B53165475153852db8b872',
+      '0xB476A5A152de86C9764454ba677DEF3BC5312c57',
+      '0xE61c0169e97A60b5aee1452eC5cE452E990262De'
+    ]
+  );
+  await multisig.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Multi-sig wallet deployed to:", multisig.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere

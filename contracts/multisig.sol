6b1a167039a3e5e707903b15fee5b47f8e8339d4
LidoKing <kinglido203@gmail.com>
2022-03-04 11:06:50 +0800

    added function for transaction rejection

--- contracts/multisig.sol
+++ contracts/multisig.sol
@@ -6,6 +6,7 @@ contract MultiSig {
   event TransactionCreated(address indexed creator, address indexed to, uint value, uint txId);
   event TransactionConfirmed(uint txId, address by, uint currentConfirmations);
   event TransactionExecuted(uint txId);
+  event TransactionRejected(uint txId, address by);
   event ConfirmationRevoked(uint txId, address by);
   event DepositReceived(uint contractBalance);
 
@@ -13,7 +14,7 @@ contract MultiSig {
 
   uint nextTxId;
   // no need to use uint8 as there are no other variables for packing
-  uint requiredConfirmations = 3;
+  uint threshold = 3;
 
   // Re-entrancy guard
   bool locked;
@@ -23,12 +24,15 @@ contract MultiSig {
     address to;
     uint value; // in ETH
     uint confirmations;
+    uint rejections;
     bool executed;
+    bool rejected;
   }
 
   mapping(uint => Transaction) idToTx;
   mapping(address => bool) public isMember;
   mapping(uint => mapping(address => bool)) confirmedByMember;
+  mapping(uint => mapping(address => bool)) rejectedByMember;
 
   // Check if function caller is member of the group
   modifier onlyOwner() {
@@ -38,26 +42,34 @@ contract MultiSig {
 
   // Check if member has confirmed transaction or not
   modifier notConfirmed(uint _txId) {
-    require(_txId < nextTxId, "Transaction does not exist.");
     require(!confirmedByMember[_txId][msg.sender], "You have already confirmed the transaction.");
     _;
   }
 
   modifier confirmed(uint _txId) {
-    require(_txId < nextTxId, "Transaction does not exist.");
     require(confirmedByMember[_txId][msg.sender], "You never confirmed the transaction.");
+    _;
+  }
+
+  modifier notRejected(uint _txId) {
+    require(!rejectedByMember[_txId][msg.sender], "You have already rejected the transaction.");
     _;
   }
 
   // Check if transaction is executed or not
   modifier inProgress(uint _txId) {
-    require(_txId < nextTxId, "Transaction does not exist.");
     require(!idToTx[_txId].executed, "Transaction has already been executed.");
+    require(!idToTx[_txId].rejected, "Transaction has already been rejected");
+    _;
+  }
+
+  modifier hasTx(uint _txId) {
+    require(_txId < nextTxId, "Transaction does not exist.");
     _;
   }
 
   modifier enoughConfirmations(uint _txId) {
-    require(idToTx[_txId].confirmations >= requiredConfirmations, "Not enough confirmations.");
+    require(idToTx[_txId].confirmations >= threshold, "Not enough confirmations.");
     _;
   }
 
@@ -93,7 +105,7 @@ contract MultiSig {
     // Save id to memory for multiple accesses to save gas
     uint txId = nextTxId;
 
-    Transaction memory _tx = Transaction(msg.sender, _to, amount, 1, false);
+    Transaction memory _tx = Transaction(msg.sender, _to, amount, 1, 0, false, false);
     idToTx[txId] = _tx;
     confirmedByMember[txId][msg.sender] = true;
     nextTxId++;
@@ -103,7 +115,7 @@ contract MultiSig {
     return txId;
   }
 
-  function confirmTransaction(uint _txId) onlyOwner notConfirmed(_txId) inProgress(_txId) external {
+  function confirmTransaction(uint _txId) onlyOwner notConfirmed(_txId) notRejected(_txId) inProgress(_txId) hasTx(_txId) external {
     Transaction storage _tx = idToTx[_txId];
     _tx.confirmations++;
     confirmedByMember[_txId][msg.sender] = true;
@@ -111,7 +123,7 @@ contract MultiSig {
     emit TransactionConfirmed(_txId, msg.sender, _tx.confirmations);
   }
 
-  function executeTransaction(uint _txId) onlyOwner enoughConfirmations(_txId) enoughBalance(_txId) external {
+  function executeTransaction(uint _txId) onlyOwner enoughConfirmations(_txId) enoughBalance(_txId) hasTx(_txId) external {
     require(!locked, "Re-entrancy detected.");
     locked = true;
     Transaction storage _tx = idToTx[_txId];
@@ -124,12 +136,24 @@ contract MultiSig {
     emit TransactionExecuted(_txId);
   }
 
-  function revokeConfirmation(uint _txId) onlyOwner inProgress(_txId) confirmed(_txId) external {
+  function revokeConfirmation(uint _txId) onlyOwner inProgress(_txId) confirmed(_txId) hasTx(_txId) external {
     Transaction storage _tx = idToTx[_txId];
     _tx.confirmations--;
     confirmedByMember[_txId][msg.sender] = false;
 
     emit ConfirmationRevoked(_txId, msg.sender);
+  }
+
+  function rejectTransaction(uint _txId) onlyOwner hasTx(_txId) notConfirmed(_txId) inProgress(_txId) notRejected(_txId) external {
+    Transaction storage _tx = idToTx[_txId];
+    _tx.rejections++;
+    rejectedByMember[_txId][msg.sender] = true;
+
+    if (_tx.rejections >= threshold) {
+      _tx.rejected = true;
+    }
+
+    emit TransactionRejected(_txId, msg.sender);
   }
 
   function checkMember(address _address) public view returns (bool) {

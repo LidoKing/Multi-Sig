210ae3c50cdea726a549f1c3dbe880d6361498fa
LidoKing <kinglido203@gmail.com>
2022-03-04 21:04:01 +0800

    added dot env

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require('hardhat-spdx-license-identifier');
 require('hardhat-gas-reporter');
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-web3");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,19 +22,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  /*networks: {
-    ropsten: {
-
-    },
-
-    rinkeby: {
+  networks: {
+    /*ropsten: {
 
     },
 
     mumbai: {
 
+    },*/
+
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/529aa3096e8647a0aef41c58bf5cd29f",
+      accounts: [process.env.PRIVATE_KEY]
     }
-  },*/
+  },
 
   solidity: "0.8.8",
 
